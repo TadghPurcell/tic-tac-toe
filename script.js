209452aee6f0f6d16753a9a69764c26c56670c5c
Tadgh Purcell <tadghp@gmail.com>
2023-03-13 12:13:55 +0000

    Add switch player functionality

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 'use strict';
 const gameBoard = (() => {
   const gameBoardArray = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    ['.', '.', '.'],
+    ['.', '.', '.'],
+    ['.', '.', '.'],
   ];
-  const printGameBoard = () => console.log(gameBoardArray);
-  const selectBoardCell = (row, column) => (gameBoardArray[row][column] = 'x');
+  let activePlayer = 0;
+  const switchActivePlayer = () =>
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  const printGameBoard = () =>
+    gameBoardArray.forEach(row => console.log(row.join(' ').split('\n')));
+  const selectBoardCell = (row, column) => {
+    activePlayer === 0
+      ? (gameBoardArray[row][column] = 'x')
+      : (gameBoardArray[row][column] = 'o');
+    printGameBoard();
+    switchActivePlayer();
+    console.log(activePlayer);
+  };
   return { gameBoardArray, printGameBoard, selectBoardCell };
 })();
 
 gameBoard.printGameBoard();
 
-const Player = name => {
-  console.log(name);
+const Player = (name, number) => {
+  return { name, number };
 };
+
+const tadgh = Player('Tadgh', 0);
+const playerTwo = Player(undefined, 1);
+
+console.log(tadgh.number);
+console.log(playerTwo.number);
