32288ecefbcdb824799e95861e33d91166025113
Tadgh Purcell <tadghp@gmail.com>
2023-03-13 11:04:50 +0000

    Create gameboard and print to console

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+const gameBoard = (() => {
+  const gameBoardArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  const printGameBoard = () => console.log(gameBoardArray);
+  return { printGameBoard };
+})();
+
+gameBoard.printGameBoard();
