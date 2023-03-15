dff4dd0c33dcc3669dcc2c26f7d710b910743663
Tadgh Purcell <tadghp@gmail.com>
2023-03-15 09:22:51 +0000

    Refactor code to sort out which modules store each piece of code

--- script.js
+++ script.js
@@ -1,74 +1,151 @@
 'use strict';
-const btnStart = document.querySelector('.btn-form');
-const btnRestart = document.querySelector('.btn-restart');
-const twoPlayerForm = document.querySelector('.start-two-player');
-const gameBoardEl = document.querySelector('.gameboard');
-const scoreboardPlayerOneEl = document.querySelector('.scoreboard-player-one');
-const scoreboardPlayerTwoEl = document.querySelector('.scoreboard-player-two');
 
 const gameBoard = (() => {
+  const gameBoardEl = document.querySelector('.gameboard'); //gameboard
   let gameBoardArray = [
     ['.', '.', '.'],
     ['.', '.', '.'],
     ['.', '.', '.'],
   ];
 
-  let activePlayer = 0;
-  const playerNames = [];
-  let playerScores = [0, 0];
-  const scoreboardOneName = document.createElement('p');
-  const scoreboardTwoName = document.createElement('p');
-  const scoreboardOneScore = document.createElement('p');
-  const scoreboardTwoScore = document.createElement('p');
+  const selectBoardCell = (row, column) => {
+    if (gameBoardArray[row][column] === '.') {
+      game.activePlayer === 0
+        ? (gameBoardArray[row][column] = 'x')
+        : (gameBoardArray[row][column] = 'o');
+      game.switchActivePlayer();
+      game.checkGameOver();
+    } else return 'Error';
+  }; //gameboard
 
-  const playerOneWins = () => playerScores[0]++;
-  const playerTwoWins = () => playerScores[1]++;
+  const printGameBoard = () => {
+    gameBoardArray.flat().forEach((_, i) => {
+      const cellSquare = document.createElement('button');
+      cellSquare.classList.add('cell');
+      cellSquare.setAttribute('index', i);
+      cellSquare.addEventListener('click', function (e) {
+        console.log(gameBoard.gameBoardArray);
+        if (e.target.attributes.index.value === '0') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(0, 0);
+        }
+        if (e.target.attributes.index.value === '1') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(0, 1);
+        }
+        if (e.target.attributes.index.value === '2') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(0, 2);
+        }
+        if (e.target.attributes.index.value === '3') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(1, 0);
+        }
+        if (e.target.attributes.index.value === '4') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(1, 1);
+        }
+        if (e.target.attributes.index.value === '5') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(1, 2);
+        }
+        if (e.target.attributes.index.value === '6') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(2, 0);
+        }
+        if (e.target.attributes.index.value === '7') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(2, 1);
+        }
+        if (e.target.attributes.index.value === '8') {
+          displayScreen.updateTextContent(e);
+          return gameBoard.selectBoardCell(2, 2);
+        }
+      });
+      gameBoard.gameBoardEl.appendChild(cellSquare);
+    });
+  };
 
-  const switchActivePlayer = () =>
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  return {
+    gameBoardArray,
+    gameBoardEl,
+    printGameBoard,
+    selectBoardCell,
+  };
+})();
+
+const displayScreen = (() => {
+  const twoPlayerForm = document.querySelector('.start-two-player'); // display
+  const btnStart = document.querySelector('.btn-form'); // display
+  // const btnRestart = document.querySelector('.btn-restart'); // display
+  const scoreboardPlayerOneEl = document.querySelector(
+    '.scoreboard-player-one'
+  ); // display
+  const scoreboardPlayerTwoEl = document.querySelector(
+    '.scoreboard-player-two'
+  ); // display
+  const scoreboardOneName = document.createElement('p'); //display
+  const scoreboardTwoName = document.createElement('p'); //display
+  const scoreboardOneScore = document.createElement('p'); //display
+  const scoreboardTwoScore = document.createElement('p'); // display
+
+  const updateScores = () => {
+    scoreboardOneScore.textContent = `${game.playerScores[0]}`;
+    scoreboardTwoScore.textContent = `${game.playerScores[1]}`;
+  }; //display
 
   const clearBoard = () => {
-    gameBoardArray = gameBoardArray.map(row => row.map(cell => (cell = '.')));
-    gameBoardEl.innerHTML = '';
+    gameBoard.gameBoardArray = gameBoard.gameBoardArray.map(row =>
+      row.map(cell => (cell = '.'))
+    );
+    gameBoard.gameBoardEl.innerHTML = '';
     updateScores();
-    activePlayer = 0;
-  };
-  const updateScores = () => {
-    scoreboardOneScore.textContent = `${gameBoard.playerScores[0]}`;
-    scoreboardTwoScore.textContent = `${gameBoard.playerScores[1]}`;
-  };
+    game.activePlayer = 0;
+  }; //display
+
   const populateScoreboards = function () {
-    scoreboardOneName.textContent = `${gameBoard.playerNames[0]}`;
-    scoreboardTwoName.textContent = `${gameBoard.playerNames[1]}`;
+    scoreboardOneName.textContent = `${game.playerNames[0]}`;
+    scoreboardTwoName.textContent = `${game.playerNames[1]}`;
     updateScores();
 
     scoreboardPlayerOneEl.appendChild(scoreboardOneName);
     scoreboardPlayerOneEl.appendChild(scoreboardOneScore);
     scoreboardPlayerTwoEl.appendChild(scoreboardTwoName);
     scoreboardPlayerTwoEl.appendChild(scoreboardTwoScore);
-  };
+  }; // display
+
+  const updateTextContent = e => {
+    if (!e.target.textContent) {
+      game.activePlayer === 0
+        ? (e.target.textContent = 'x')
+        : (e.target.textContent = 'o');
+    }
+  }; //display
 
   const resetGame = function () {
     // display form
     twoPlayerForm.classList.remove('hidden');
 
     //hide screen
-    gameBoardEl.classList.add('hidden');
-    btnRestart.classList.add('hidden');
+    gameBoard.gameBoardEl.classList.add('hidden');
+    game.btnRestart.classList.add('hidden');
     scoreboardPlayerOneEl.classList.add('hidden');
     scoreboardPlayerTwoEl.classList.add('hidden');
 
-    playerNames.pop();
-    playerNames.pop();
+    game.playerNames.pop();
+    game.playerNames.pop();
+
+    game.playerScores = game.playerScores.map(score => (score = 0));
 
     scoreboardPlayerOneEl.removeChild(scoreboardOneName);
     scoreboardPlayerTwoEl.removeChild(scoreboardTwoName);
-  };
+  }; //
 
   const revertToMainMenu = function () {
     clearBoard();
     resetGame();
-  };
+  }; //
+
   const toggleModal = content => {
     const overlay = document.querySelector('.overlay');
     const overlayText = document.querySelector('.overlay-text');
@@ -83,32 +160,83 @@ const gameBoard = (() => {
       overlayText.innerHTML = '';
       overlay.classList.add('hidden');
       overlayText.classList.add('hidden');
-      clearBoard();
-      printGameBoard();
+      displayScreen.clearBoard();
+      gameBoard.printGameBoard();
     };
     overlay.addEventListener('click', clearOverlay);
     overlayText.addEventListener('click', clearOverlay);
-  };
+  }; //display
 
-  const checkGameOver = () => {
-    if (gameBoardArray.flat().every(el => el !== '.')) {
-      toggleModal('Its a Draw!');
-    }
+  btnStart.addEventListener('click', function (e) {
+    e.preventDefault();
+    // hide form
+    twoPlayerForm.classList.add('hidden');
+
+    //display screen
+    gameBoard.gameBoardEl.classList.remove('hidden');
+    game.btnRestart.classList.remove('hidden');
+    scoreboardPlayerOneEl.classList.remove('hidden');
+    scoreboardPlayerTwoEl.classList.remove('hidden');
+
+    //input names
+    game.getPlayerNames();
+    displayScreen.populateScoreboards();
+
+    console.log(document.querySelector('#player-one').value);
+    console.log(document.querySelector('#player-two').value);
+    gameBoard.printGameBoard();
+  }); //display
+  return {
+    populateScoreboards,
+    clearBoard,
+    updateTextContent,
+    toggleModal,
+    revertToMainMenu,
   };
+})();
+
+const game = (() => {
+  const btnRestart = document.querySelector('.btn-restart'); // display
+
+  let activePlayer = 0; // game
+  let playerScores = [0, 0]; // game
+
+  const playerNames = []; // game
+
+  btnRestart.addEventListener('click', displayScreen.revertToMainMenu); // display
+
+  const playerOneWins = () => playerScores[0]++; //game
+  const playerTwoWins = () => playerScores[1]++; //game
+
+  const getPlayerNames = function () {
+    game.playerNames.push(
+      document.getElementById('player-one').value || 'Player One',
+      document.getElementById('player-two').value || 'Player Two'
+    );
+  }; //game
+
+  const switchActivePlayer = () =>
+    game.activePlayer === 0 ? (game.activePlayer = 1) : (game.activePlayer = 0); //game
+
+  const checkDraw = () => {
+    if (gameBoard.gameBoardArray.flat().every(el => el !== '.')) {
+      displayScreen.toggleModal('Its a Draw!');
+    }
+  }; //game
 
   const checkWinnerHorizontal = () =>
-    gameBoardArray.forEach(row => {
+    gameBoard.gameBoardArray.forEach(row => {
       if (row.every(el => el === 'x')) {
         playerOneWins();
-        toggleModal('You Win');
+        displayScreen.toggleModal('You Win');
       } else if (row.every(el => el === 'o')) {
         playerTwoWins();
-        toggleModal('you lose');
+        displayScreen.toggleModal('you lose');
       }
-    });
+    }); //game
 
   const checkWinnerVertical = () => {
-    const updatedBoard = gameBoardArray.flat();
+    const updatedBoard = gameBoard.gameBoardArray.flat();
     for (let i = 0; i < updatedBoard.length; i++) {
       if (
         updatedBoard[i] === 'x' &&
@@ -116,20 +244,20 @@ const gameBoard = (() => {
         updatedBoard[i + 6] === 'x'
       ) {
         playerOneWins();
-        toggleModal('you win');
+        displayScreen.toggleModal('you win');
       } else if (
         updatedBoard[i] === 'o' &&
         updatedBoard[i + 3] === 'o' &&
         updatedBoard[i + 6] === 'o'
       ) {
         playerTwoWins();
-        toggleModal('you lose');
+        displayScreen.toggleModal('you lose');
       }
     }
-  };
+  }; //game
 
   const checkWinnerDiagonal = () => {
-    const updatedBoard = gameBoardArray.flat();
+    const updatedBoard = gameBoard.gameBoardArray.flat();
     if (
       (updatedBoard[0] === 'x' &&
         updatedBoard[4] === 'x' &&
@@ -139,7 +267,7 @@ const gameBoard = (() => {
         updatedBoard[6] === 'x')
     ) {
       playerOneWins();
-      toggleModal('you win');
+      displayScreen.toggleModal('you win');
     } else if (
       (updatedBoard[0] === 'o' &&
         updatedBoard[4] === 'o' &&
@@ -149,117 +277,23 @@ const gameBoard = (() => {
         updatedBoard[6] === 'o')
     ) {
       playerTwoWins();
-      toggleModal('you lose');
+      displayScreen.toggleModal('you lose');
     }
-  };
+  }; //game
 
-  const selectBoardCell = (row, column) => {
-    if (gameBoardArray[row][column] === '.') {
-      activePlayer === 0
-        ? (gameBoardArray[row][column] = 'x')
-        : (gameBoardArray[row][column] = 'o');
-      switchActivePlayer();
-      checkWinnerHorizontal();
-      checkWinnerVertical();
-      checkWinnerDiagonal();
-      checkGameOver();
-    } else return 'Error';
-  };
-
-  const updateTextContent = e => {
-    if (!e.target.textContent) {
-      activePlayer === 0
-        ? (e.target.textContent = 'x')
-        : (e.target.textContent = 'o');
-    }
-  };
-
-  const printGameBoard = () => {
-    gameBoardArray.flat().forEach((_, i) => {
-      const cellSquare = document.createElement('button');
-      cellSquare.classList.add('cell');
-      cellSquare.setAttribute('index', i);
-      cellSquare.addEventListener('click', function (e) {
-        if (e.target.attributes.index.value === '0') {
-          updateTextContent(e);
-          return selectBoardCell(0, 0);
-        }
-        if (e.target.attributes.index.value === '1') {
-          updateTextContent(e);
-          return selectBoardCell(0, 1);
-        }
-        if (e.target.attributes.index.value === '2') {
-          updateTextContent(e);
-          return selectBoardCell(0, 2);
-        }
-        if (e.target.attributes.index.value === '3') {
-          updateTextContent(e);
-          return selectBoardCell(1, 0);
-        }
-        if (e.target.attributes.index.value === '4') {
-          updateTextContent(e);
-          return selectBoardCell(1, 1);
-        }
-        if (e.target.attributes.index.value === '5') {
-          updateTextContent(e);
-          return selectBoardCell(1, 2);
-        }
-        if (e.target.attributes.index.value === '6') {
-          updateTextContent(e);
-          return selectBoardCell(2, 0);
-        }
-        if (e.target.attributes.index.value === '7') {
-          updateTextContent(e);
-          return selectBoardCell(2, 1);
-        }
-        if (e.target.attributes.index.value === '8') {
-          updateTextContent(e);
-          return selectBoardCell(2, 2);
-        }
-      });
-      gameBoardEl.appendChild(cellSquare);
-    });
-  };
-
+  const checkGameOver = () => {
+    checkWinnerHorizontal();
+    checkWinnerVertical();
+    checkWinnerDiagonal();
+    checkDraw();
+  }; //game
   return {
-    printGameBoard,
-    selectBoardCell,
-    clearBoard,
-    gameBoardArray,
+    btnRestart,
+    activePlayer,
     playerNames,
-    revertToMainMenu,
-    populateScoreboards,
+    getPlayerNames,
     playerScores,
+    switchActivePlayer,
+    checkGameOver,
   };
-})();
-
-const displayScreen = (() => {
-  btnRestart.addEventListener('click', gameBoard.revertToMainMenu);
-  const getPlayerNames = function () {
-    gameBoard.playerNames.push(
-      document.getElementById('player-one').value || 'Player One',
-      document.getElementById('player-two').value || 'Player Two'
-    );
-  };
-
-  btnStart.addEventListener('click', function (e) {
-    e.preventDefault();
-    // hide form
-    twoPlayerForm.classList.add('hidden');
-
-    //display screen
-    gameBoardEl.classList.remove('hidden');
-    btnRestart.classList.remove('hidden');
-    scoreboardPlayerOneEl.classList.remove('hidden');
-    scoreboardPlayerTwoEl.classList.remove('hidden');
-
-    //input names
-    getPlayerNames();
-    gameBoard.populateScoreboards();
-
-    console.log(document.querySelector('#player-one').value);
-    console.log(document.querySelector('#player-two').value);
-    gameBoard.printGameBoard();
-  });
-  return {};
 })();
